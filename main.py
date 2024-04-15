174c26e886aea89a32d3ae02f519331767fc5f6b
Dany <danyanoxar@gmail.com>
2024-04-15 08:19:51 +0300

    1 - create decorator witch printing word - "Gambler".

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 
+def decorator(func):
+    def wrap(*args, **kwargs):  # Определние выполняемой функции
+        print('Gambler')
+        func() # Вызов функции декоратора
+        return 0
+    return wrap
+
+@decorator
 def main():
     print('HEL')
+
+
+
+
 
 if __name__=="__main__":
     main()
